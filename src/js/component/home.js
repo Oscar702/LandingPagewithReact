481356cb088c28f2fce3fc35edd2c0e4f08ebaf0
Oscar702 <FunesLV@aol.com>
2021-01-07 16:45:35 +0000

    getting there

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { MyCard } from "./mycard.js";
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import { MyNavBar } from "./MyNavBar.js";
+import { MainSection } from "./MainSection";
 
 export function Home() {
 	return (
 		<div>
+			<MyNavBar />
+			<MainSection />
 			<h1>Hello World</h1>
-			<h2>hiiii</h2> <MyCard />
+			<h2>hiiii</h2>
+			<div>
+				<MyCard Img="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg" />
+				<MyCard Img="https://hips.hearstapps.com/countryliving.cdnds.net/17/47/1511194376-cavachon-puppy-christmas.jpg" />
+				<MyCard />
+				<MyCard />
+			</div>
 		</div>
 	);
 }
--- src/js/component/mycard.js
+++ src/js/component/mycard.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
+import PropTypes from "prop-types";
 
-export function MyCard() {
-	return <h1>test</h1>;
+export function MyCard(props) {
+	return (
+		<Card style={{ width: "18rem", display: "inline-block" }}>
+			<Card.Img variant="top" src={props.Img} />
+			<Card.Body>
+				<Card.Title>4GEEKS Academy project</Card.Title>
+				<Card.Text />
+				<Button variant="primary">Go somewhere</Button>
+			</Card.Body>
+		</Card>
+	);
 }
+MyCard.propTypes = {
+	Img: PropTypes.string
+};
